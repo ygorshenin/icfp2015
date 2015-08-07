9f6f724ac3c73cae2c9d68f0469f16e030d67401
Yuri Gorshenin <mipt.vi002@gmail.com>
2015-08-07 23:49:22 +0300

    [renderer] Simple CL parsing.

--- renderer/Main.hs
+++ renderer/Main.hs
@@ -1,17 +1,26 @@
 module Main where
 
 import Control.Monad
+import Control.Monad.State
 import Data.IORef
 import Graphics.Rendering.OpenGL (($=), GLfloat)
+import System.Environment
 import System.Exit
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLFW as GLFW
 
-data RendererState = RendererState { rsQuit :: Bool }
+data RendererState = RendererState { rsQuit :: Bool
+                                   , rsNumRows :: Int
+                                   , rsNumCols :: Int
+                                   }
                    deriving (Show, Eq)
 
 setQuit :: RendererState -> RendererState
 setQuit rs = rs { rsQuit = True }
+
+setNumRows, setNumCols :: Int -> RendererState -> RendererState
+setNumRows nr rs = rs { rsNumRows = nr }
+setNumCols nc rs = rs { rsNumCols = nc }
 
 windowWidth = 800
 windowHeight = 600
@@ -55,9 +64,13 @@ drawHexagon = do
                 color 1.0 1.0 1.0
                 forM_ hexagon $ \(x, y) -> vertex x y
 
-drawGrid :: (Int, Int) -> IO ()
-drawGrid (numRows, numCols) = do
-  let dx = 2.0 / (fromIntegral numCols + 0.5) :: GLfloat
+drawGrid :: IORef RendererState -> IO ()
+drawGrid rendererState = do
+  rs <- readIORef rendererState
+  let numRows = rsNumRows rs
+      numCols = rsNumCols rs
+
+      dx = 2.0 / (fromIntegral numCols + 0.5) :: GLfloat
       dy = 2.0 / (fromIntegral numRows) :: GLfloat
       dist = min dx (2.0 * dy / sqrt 3.0)
       distX = dist
@@ -72,26 +85,38 @@ drawGrid (numRows, numCols) = do
       drawHexagon
   return ()
 
-display :: IO ()
-display = do
+display :: IORef RendererState -> IO ()
+display rs = do
   GL.clearColor $= GL.Color4 0 0 0 1.0
   GL.clear [GL.ColorBuffer]
 
   GL.loadIdentity
-  drawGrid (20, 10)
+  drawGrid rs
 
 rendererLoop :: IORef RendererState -> IO ()
 rendererLoop rendererState = do
   let loop = do
         GLFW.pollEvents
-        display
+        display rendererState
         GLFW.swapBuffers
         rs <- readIORef rendererState
         when (not $ rsQuit rs) $ loop
   loop
 
+type CLI = State [String]
+
+parseArgs :: RendererState -> [String] -> RendererState
+parseArgs rs [] = rs
+parseArgs rs (s:ss) =
+    case s of
+      "-h" -> parseArgs (rs { rsNumRows = (read $ head ss) }) (tail ss)
+      "-w" -> parseArgs (rs { rsNumCols = (read $ head ss) }) (tail ss)
+      _    -> parseArgs rs ss
+
 main :: IO ()
 main = do
+  args <- getArgs
+
   runOrDie GLFW.initialize "GLFW: can't initialize"
   runOrDie (GLFW.openWindow
             (GL.Size windowWidth windowHeight)
@@ -100,7 +125,7 @@ main = do
        "GLFW: can't create window"
   GLFW.windowTitle $= "IFPC2015"
 
-  rendererState <- newIORef $ RendererState False
+  rendererState <- newIORef $ parseArgs (RendererState False 1 1) args
   GLFW.windowCloseCallback $= onClose rendererState
   GLFW.keyCallback $= onKey rendererState
   rendererLoop rendererState
